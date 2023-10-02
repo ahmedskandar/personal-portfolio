d0a64fd2ed2177478330c3aa54f2caab128ae1d9
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-10-02 09:36:26 +0300

    take #4 of trying to fix  mobile overflow error of hero container

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -4,44 +4,44 @@ import HeroFooter from "./HeroFooter";
 import HeroContent from "./HeroContent";
 
 function HeroSection() {
-  const [sectionHeight, setSectionHeight] = useState("100vh");
+  // const [sectionHeight, setSectionHeight] = useState("100vh");
 
-  useEffect(() => {
-    // Function to calculate and set the section's height
-    const setHeight = () => {
-      const windowHeight =
-        window.innerHeight ||
-        document.documentElement.clientHeight ||
-        document.body.clientHeight;
-      const screenAvailableHeight =
-        window.screen.availHeight || window.screen.height;
-      const approxTopBarHeight = screenAvailableHeight - windowHeight;
+  // useEffect(() => {
+  //   // Function to calculate and set the section's height
+  //   const setHeight = () => {
+  //     const windowHeight =
+  //       window.innerHeight ||
+  //       document.documentElement.clientHeight ||
+  //       document.body.clientHeight;
+  //     const screenAvailableHeight =
+  //       window.screen.availHeight || window.screen.height;
+  //     const approxTopBarHeight = screenAvailableHeight - windowHeight;
 
-      // Set the section height, subtracting an approximation of the top bar height
-      setSectionHeight(`calc(100vh - ${approxTopBarHeight}px)`);
-    };
+  //     // Set the section height, subtracting an approximation of the top bar height
+  //     setSectionHeight(`calc(100vh - ${approxTopBarHeight}px)`);
+  //   };
 
-    // Initial call to set the height
-    setHeight();
+  //   // Initial call to set the height
+  //   setHeight();
 
-    // Attach a resize event listener to handle changes in height
-    window.addEventListener("resize", setHeight);
+  //   // Attach a resize event listener to handle changes in height
+  //   window.addEventListener("resize", setHeight);
 
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("resize", setHeight);
-    };
-  }, []);
-  console.log(sectionHeight ? sectionHeight : "not rendered");
-  const sectionStyle = {
-    height: sectionHeight,
-  };
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("resize", setHeight);
+  //   };
+  // }, []);
+  // console.log(sectionHeight ? sectionHeight : "not rendered");
+  // const sectionStyle = {
+  //   height: sectionHeight,
+  // };
 
   return (
     <section
       alt="Dark background image displaying a mouse"
-      style={sectionStyle}
-      className="w-full flex flex-col justify-between bg-cover bg-no-repeat bg-[95%] bg-[url('../public/assets/images/landing-page-mobile.webp')] md:bg-[url('../public/assets/images/landing-page-bg.webp')] md:bg-fixed md:!h-screen"
+      // style={sectionStyle}
+      className="w-full h-[90vh] flex flex-col justify-between bg-cover bg-no-repeat bg-[95%] bg-[url('../public/assets/images/landing-page-mobile.webp')] md:bg-[url('../public/assets/images/landing-page-bg.webp')] md:bg-fixed md:h-screen"
     >
       <HeroHeader />
       <HeroContent />
