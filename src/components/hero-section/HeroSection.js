ed63dbedffb02a8b93ea85baf33394b732f54ddf
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-10-02 09:31:43 +0300

    take #3 of trying to fix  mobile overflow error of hero container

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -41,7 +41,7 @@ function HeroSection() {
     <section
       alt="Dark background image displaying a mouse"
       style={sectionStyle}
-      className="w-full relative bg-cover bg-no-repeat bg-[95%] bg-[url('../public/assets/images/landing-page-mobile.webp')] md:bg-[url('../public/assets/images/landing-page-bg.webp')] md:bg-fixed md:h-screen"
+      className="w-full flex flex-col justify-between bg-cover bg-no-repeat bg-[95%] bg-[url('../public/assets/images/landing-page-mobile.webp')] md:bg-[url('../public/assets/images/landing-page-bg.webp')] md:bg-fixed md:!h-screen"
     >
       <HeroHeader />
       <HeroContent />
